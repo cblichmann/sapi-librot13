fe67cd38e62acedcab1b3801ffaf36b0edc72f06
Christian Blichmann <cblichmann@google.com>
2020-07-16 11:45:21 +0200

    Modify the policy to map input and output files
    This re-enables the mount namespace feature and maps both the input and
    output files.
    
    The input file is mapped as is, the output file is placed under
    `/output/<OUTFILE_BASENAME>`.
    
    Signed-off-by: Christian Blichmann <cblichmann@google.com>

--- main_sandboxed.cc
+++ main_sandboxed.cc
@@ -14,13 +14,22 @@ ABSL_DECLARE_FLAG(string, sandbox2_danger_danger_permit_all_and_log);
 // sandboxee will have a different working directory.
 class Rot13SapiSandbox : public Rot13Sandbox {
  public:
+  Rot13SapiSandbox(const std::string& in_file, const std::string& out_file)
+      : in_file_(in_file), out_file_(out_file) {}
+
   std::unique_ptr<sandbox2::Policy> ModifyPolicy(
       sandbox2::PolicyBuilder*) override {
     // Return a new policy.
     return sandbox2::PolicyBuilder()
-        .DisableNamespaces()
+        .AddFile(in_file_)
+        // Why are we adding a directory here?
+        .AddDirectoryAt(dirname(&out_file_[0]), "/output", /*is_ro=*/false)
         .BuildOrDie();
   }
+
+ private:
+  std::string in_file_;
+  std::string out_file_;
 };
 
 int main(int argc, char* argv[]) {
@@ -35,7 +44,7 @@ int main(int argc, char* argv[]) {
   std::string out_file(argv[2]);
 
   std::cout << "initializing sandbox...\n";
-  Rot13SapiSandbox sandbox;
+  Rot13SapiSandbox sandbox(in_file, out_file);
   sandbox.Init().IgnoreError();
 
   std::cout << "obfuscating " << in_file << ", saving to " << out_file
@@ -43,7 +52,9 @@ int main(int argc, char* argv[]) {
 
   Rot13Api api(&sandbox);
   sapi::v::ConstCStr in_file_var(in_file.c_str());
-  sapi::v::ConstCStr out_file_var(out_file.c_str());
+  std::string out_file_in_sandboxee(std::string("/output/") +
+                                    basename(&out_file[0]));
+  sapi::v::ConstCStr out_file_var(out_file_in_sandboxee.c_str());
 
   std::cout << "result: "
             << api.Rot13File(in_file_var.PtrBefore(), out_file_var.PtrBefore())
